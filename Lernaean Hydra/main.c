1a8ecea887c7cfa3893dd4c8b27fc25f3fc11877
suedadam <suedadam@gmail.com>
2018-03-02 16:54:58 -0800

    Clean up

--- Lernaean Hydra/main.c
+++ Lernaean Hydra/main.c
@@ -6,7 +6,7 @@
 /*   By: asyed <asyed@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 14:00:01 by asyed             #+#    #+#             */
-/*   Updated: 2018/03/02 16:09:41 by asyed            ###   ########.fr       */
+/*   Updated: 2018/03/02 16:54:28 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,38 +35,17 @@ static int	enabled(const char *file)
 	return ((int)(buf[0] - '0'));
 }
 
-/*
- * in_cksum --
- * Checksum routine for Internet Protocol
- * family headers (C Version)
- */
-unsigned short in_cksum(unsigned short *addr, int len)
+u_short	csum(u_short *addr, size_t len)
 {
-    register int sum = 0;
-    u_short answer = 0;
-    register u_short *w = addr;
-    register int nleft = len;
-    /*
-     * Our algorithm is simple, using a 32 bit accumulator (sum), we add
-     * sequential 16 bit words to it, and at the end, fold back all the
-     * carry bits from the top 16 bits into the lower 16 bits.
-     */
-    while (nleft > 1)
-    {
-      sum += *w++;
-      nleft -= 2;
-    }
-    /* mop up an odd byte, if necessary */
-    if (nleft == 1)
-    {
-      *(u_char *) (&answer) = *(u_char *) w;
-      sum += answer;
-    }
-    /* add back carry outs from top 16 bits to low 16 bits */
-    sum = (sum >> 16) + (sum & 0xffff);       /* add hi 16 to low 16 */
-    sum += (sum >> 16);               /* add carry */
-    answer = ~sum;              /* truncate to 16 bits */
-    return (answer);
+	unsigned long	sum = 0;
+	while (len > 0)
+	{
+		sum += *addr++;
+		len--;
+	}
+	sum = (sum >> 16) + (sum & 0xFFFF);
+	sum += (sum >> 16);
+	return ((unsigned short)~sum);
 }
 
 int	main(void)
@@ -117,9 +96,9 @@ int	main(void)
 			icmppacket->icmp_type = ICMP_ECHOREPLY;
 			icmppacket->icmp_id = ((struct icmp *)(buf + sizeof(struct ip)))->icmp_id;
 			icmppacket->icmp_cksum = 0;
-			icmppacket->icmp_cksum = in_cksum((unsigned short *)icmppacket, packet_size - sizeof(struct ip));
+			icmppacket->icmp_cksum = csum((unsigned short *)icmppacket, packet_size - sizeof(struct ip));
 			ipreply->ip_sum = 0;
-			ipreply->ip_sum = in_cksum((unsigned short *)ipreply, sizeof(struct ip));
+			ipreply->ip_sum = csum((unsigned short *)ipreply, sizeof(struct ip));
 			conn.sin_family = AF_INET;
 			conn.sin_addr.s_addr = ipreply->ip_dst.s_addr;
 			if (ret == 1)
@@ -127,9 +106,6 @@ int	main(void)
 					printf("Failed to send %s\n", strerror(errno));
 			if (sendto(s_sock, packet, packet_size, 0, (struct sockaddr *)&conn, sizeof(struct sockaddr)) < 0)
 				printf("Failed to send %s\n", strerror(errno));
-			// printf("Received:\n\tIPHL: %d, IPV: %d, IPTOS: %d, IPLEN: %d, IP_ID: %d, IP_OFF: %d, IP_TTL: %d, IP_P: %d, IP_SUM: %d\n\tICMP: Type: %d, Code: %d, cksum: %d, data: \"%s\"\n", ((struct ip *)buf)->ip_hl, ((struct ip *)buf)->ip_v, ((struct ip *)buf)->ip_tos, ((struct ip *)buf)->ip_len, ((struct ip *)buf)->ip_id, ((struct ip *)buf)->ip_off, ((struct ip *)buf)->ip_ttl, ((struct ip *)buf)->ip_p, ((struct ip *)buf)->ip_sum, ((struct icmp *)(buf + sizeof(struct ip)))->icmp_type, ((struct icmp *)(buf + sizeof(struct ip)))->icmp_code, ((struct icmp *)(buf + sizeof(struct ip)))->icmp_cksum, ((struct icmp *)(buf + sizeof(struct ip)))->icmp_data);
 		}
-		// else
-		// 	printf("PONG:\nIPHL: %d, IPV: %d, IPTOS: %d, IPLEN: %d, IP_ID: %d, IP_OFF: %d, IP_TTL: %d, IP_P: %d, IP_SUM: %d\nICMP: Type: %d, Code: %d, cksum: %d, data: \"%s\"\n", ((struct ip *)buf)->ip_hl, ((struct ip *)buf)->ip_v, ((struct ip *)buf)->ip_tos, ((struct ip *)buf)->ip_len, ((struct ip *)buf)->ip_id, ((struct ip *)buf)->ip_off, ((struct ip *)buf)->ip_ttl, ((struct ip *)buf)->ip_p, ((struct ip *)buf)->ip_sum, ((struct icmp *)(buf + sizeof(struct ip)))->icmp_type, ((struct icmp *)(buf + sizeof(struct ip)))->icmp_code, ((struct icmp *)(buf + sizeof(struct ip)))->icmp_cksum, ((struct icmp *)(buf + sizeof(struct ip)))->icmp_data);
 	}
 }
