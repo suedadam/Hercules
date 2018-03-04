327ea7bf9db7fc4acd0d0cecb817354b0bcc181f
suedadam <suedadam@gmail.com>
2018-03-03 18:07:47 -0800

    Norm :/ its ugly now

--- Lernaean Hydra/main.c
+++ Lernaean Hydra/main.c
@@ -3,107 +3,98 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asyed <asyed@student.42.us.org>            +#+  +:+       +#+        */
+/*   By: asyed <asyed@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 14:00:01 by asyed             #+#    #+#             */
-/*   Updated: 2018/03/03 12:04:25 by asyed            ###   ########.fr       */
+/*   Updated: 2018/03/03 18:01:51 by asyed            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define _GNU_SOURCE
+#include "lernaen.h"
 
-#include <stdio.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <string.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <netinet/ip.h>
-#include <netinet/ip_icmp.h>
-#include <unistd.h>
-
-static int	enabled(const char *file)
+static int		p_listen(t_packet_info *packet)
 {
-	FILE	*fp;
-	char	buf[10];
-
-	if ((fp = fopen(file, "r")) == NULL)
-		return (-1);
-	fread(buf, sizeof(char), 1, fp);
-	return ((int)(buf[0] - '0'));
-}
-
-u_short	csum(u_short *addr, size_t len)
-{
-	unsigned long	sum = 0;
-	while (len > 0)
-	{
-		sum += *addr++;
-		len--;
-	}
-	sum = (sum >> 16) + (sum & 0xFFFF);
-	sum += (sum >> 16);
-	return ((unsigned short)~sum);
-}
-
-int	main(int argc, char *argv[])
-{
-	int					s_sock;
-	int					ret;
-	int					kern;
-	struct icmp			*icmppacket;
-	struct ip			*ipreply;
-	struct sockaddr_in	conn;
-	void				*packet;
-	size_t				packet_size;
-
-	if (argc >= 2 && !strcmp(argv[1], "-D"))
-		if ((ret = daemon(0, 0)) == -1)
-			return (ret);
-	if ((s_sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1)
+	if ((packet->s_sock = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1)
 	{
 		printf("socket(%s)\n", strerror(errno));
-		return (s_sock);
-	}
-	if (!(packet = malloc(1024)))
 		return (-1);
-	bzero(packet, 1024);
-	ipreply = (struct ip *)packet;
-	icmppacket = (struct icmp *)(packet + sizeof(struct ip));
-	if ((ret = enabled("/proc/sys/net/ipv4/icmp_echo_ignore_all")) == -1)
+	}
+	if ((packet->ret =
+		enabled("/proc/sys/net/ipv4/icmp_echo_ignore_all")) == -1)
 	{
 		printf("Failed to read sys file for ICMP!!\n");
-		ret = 1;
+		packet->ret = 1;
 	}
-	kern = 1;
-	if(setsockopt(s_sock, IPPROTO_IP, IP_HDRINCL, &kern, sizeof(int)) < 0)
+	packet->kern = 1;
+	if (setsockopt(packet->s_sock, IPPROTO_IP,
+		IP_HDRINCL, &(packet->kern), sizeof(int)) < 0)
 	{
 		printf("setsockopt(%s)\n", strerror(errno));
-		exit(-1);
+		return (-1);
 	}
-	while (recv(s_sock, packet, 1024, 0))
+	return (0);
+}
+
+static int		copy_ipheader(t_packet_info *packet)
+{
+	uint32_t	tmp;
+
+	packet->ippacket = (struct ip *)packet->packetbuf;
+	packet->icmppacket = (struct icmp *)(packet->packetbuf
+							+ sizeof(struct ip));
+	tmp = 0;
+	tmp = packet->ippacket->ip_src.s_addr;
+	memcpy(&(packet->ippacket->ip_src.s_addr),
+		&(packet->ippacket->ip_dst.s_addr), sizeof(uint32_t));
+	memcpy(&(packet->ippacket->ip_dst.s_addr), &tmp, sizeof(uint32_t));
+	return (0);
+}
+
+static int		send_packet(t_packet_info *packet)
+{
+	copy_ipheader(packet);
+	packet->packet_size = ntohs(((struct ip *)packet->packetbuf)->ip_len);
+	bzero(packet->packetbuf + packet->packet_size, 1024 - packet->packet_size);
+	packet->icmppacket->icmp_type = ICMP_ECHOREPLY;
+	packet->icmppacket->icmp_id = ((struct icmp *)
+							(packet->packetbuf + sizeof(struct ip)))->icmp_id;
+	packet->icmppacket->icmp_cksum = 0;
+	packet->icmppacket->icmp_cksum = csum((unsigned short *)packet->icmppacket,
+							packet->packet_size - sizeof(struct ip));
+	packet->ippacket->ip_sum = 0;
+	packet->ippacket->ip_sum = csum((unsigned short *)packet->ippacket,
+							sizeof(struct ip));
+	packet->conn.sin_family = AF_INET;
+	packet->conn.sin_addr.s_addr = packet->ippacket->ip_dst.s_addr;
+	if (packet->ret == 1)
+		if (sendto(packet->s_sock, packet->packetbuf, packet->packet_size,
+			0, (struct sockaddr *)&(packet->conn), sizeof(struct sockaddr)) < 0)
+			return (-1);
+	if (sendto(packet->s_sock, packet->packetbuf, packet->packet_size,
+			0, (struct sockaddr *)&(packet->conn), sizeof(struct sockaddr)) < 0)
+		return (-1);
+	return (0);
+}
+
+int				main(int argc, char *argv[])
+{
+	t_packet_info	packet;
+
+	bzero(&packet, sizeof(t_packet_info));
+	p_listen(&packet);
+	if (argc >= 2 && !strcmp(argv[1], "-D"))
+		if ((packet.ret = daemon(0, 0)) == -1)
+			return (packet.ret);
+	while (recv(packet.s_sock, packet.packetbuf, 1024, 0))
 	{
-		if (((struct icmp *)(packet + sizeof(struct ip)))->icmp_type == ICMP_ECHO)
+		if (((struct icmp *)(packet.packetbuf +
+			sizeof(struct ip)))->icmp_type == ICMP_ECHO)
 		{
-			packet_size = ntohs(((struct ip *)packet)->ip_len);
-			bzero(packet + packet_size, 1024 - packet_size);
-			memcpy(&(ipreply->ip_src.s_addr), &(((struct ip *)packet)->ip_dst.s_addr), sizeof(uint32_t));
-			memcpy(&(ipreply->ip_dst.s_addr), &(((struct ip *)packet)->ip_src.s_addr), sizeof(uint32_t));
-			icmppacket->icmp_type = ICMP_ECHOREPLY;
-			icmppacket->icmp_id = ((struct icmp *)(packet + sizeof(struct ip)))->icmp_id;
-			icmppacket->icmp_cksum = 0;
-			icmppacket->icmp_cksum = csum((unsigned short *)icmppacket, packet_size - sizeof(struct ip));
-			ipreply->ip_sum = 0;
-			ipreply->ip_sum = csum((unsigned short *)ipreply, sizeof(struct ip));
-			conn.sin_family = AF_INET;
-			conn.sin_addr.s_addr = ipreply->ip_dst.s_addr;
-			if (ret == 1)
-				if (sendto(s_sock, packet, packet_size, 0, (struct sockaddr *)&conn, sizeof(struct sockaddr)) < 0)
-					printf("Failed to send %s\n", strerror(errno));
-			if (sendto(s_sock, packet, packet_size, 0, (struct sockaddr *)&conn, sizeof(struct sockaddr)) < 0)
-				printf("Failed to send %s\n", strerror(errno));
+			if (send_packet(&packet) == -1)
+			{
+				printf("Failed to send packet(s) %s\n", strerror(errno));
+				return (-1);
+			}
 		}
 	}
 }
